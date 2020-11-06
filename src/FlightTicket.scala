155e77ee6bf2183029eb848ba1a954d228f95552
CloudHare <jan.rimantas@gmail.com>
2020-11-06 20:59:14 +0200

    Implemented FlightTicket and PackageDelivery

--- src/FlightTicket.scala
+++ src/FlightTicket.scala
@@ -2,12 +2,21 @@ import Enums._
 import Destinations._
 import FlightClasses._
 
+case class FlightTicketData(GoodDoc: Boolean, Price: Double, Luggage:Int)
+
 abstract class FlightTicket extends FlightClass with Destination {
 
-  def getTicket(doc: Document): String //change return value to FlightTicket Data
-  def calculatePrice(): Double
+  def getTicket(doc: Document): Option[FlightTicketData] = {
+    var price : Double = super[FlightClass].calculatePrice() * super[Destination].calculatePrice()
+    BigDecimal(price).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+    Some(FlightTicketData(goodDocument(doc), price, calculateLuggage()))
+  }
+/*
+  def calculatePrice(): Double // both superclasses have such method
   def calculateLuggage(): Int
   def goodDocument(doc: Document): Boolean
   def getDestination(): String
   def getFlightClass(): String
+*/
 }
+
--- src/PackageDelivery.scala
+++ src/PackageDelivery.scala
@@ -1,3 +1,10 @@
-abstract class PackageDelivery {
+import Enums._
+import Destinations._
+
+abstract class PackageDelivery extends Destination{
+
+  def calculatePrice(weight: Int): Double = {
+    super[Destination].calculatePrice() * weight * 0.02
+  }
 
 }
